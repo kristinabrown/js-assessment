2f4d4180b13bf26b3b7c05296aa639d4397bb31e
Kristina Brown <kristina.frey.frey@gmail.com>
2015-06-28 20:52:12 -0600

    WIP array exercises

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,40 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    for(i = 0; i < arr.length; i++){
+      if (arr[i] == item){
+        return i;
+      }
+    }
+    return -1;
   },
 
   sum : function(arr) {
-
+    var sum = 0;
+    for(i = 0; i < arr.length; i++){
+      sum += arr[i];
+    }
+    return sum;
   },
 
   remove : function(arr, item) {
-
+    for(i = 0; i < arr.length; i++){
+      if(arr[i] == item){
+        arr.splice(i, 1);
+      }
+    }
+    return arr
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    var new_arr = arr
+    for(i = 0; i < new_arr.length; i++){
+      if(new_arr[i] == item){
+        var nullThing = new_arr.splice(i, 1);
+        nullThing = null
+      }
+    }
+    return new_arr;
   },
 
   append : function(arr, item) {
